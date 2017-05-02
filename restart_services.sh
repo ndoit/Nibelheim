dbcbe04375860a3e0997f45f96dc6eb50aa39311
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2017-05-02 15:53:10 -0400

    start redis in background

--- restart_services.sh
+++ restart_services.sh
@@ -1,11 +1,11 @@
 # does something to the network so the host can connect to localhost
 service iptables stop
 
-# start neo4j dev server
-neos
-
-# start up redis
-redis-server
+# # start neo4j dev server
+# neos
 
 # restart elasticsearch
 service elasticsearch restart
+
+# start up redis
+redis-server /etc/redis.conf
