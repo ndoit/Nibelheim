3eee22c112ab71799195a6e6278354b054b6369e
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2017-05-05 09:55:27 -0400

    restart services script added

--- restart_services.sh
+++ restart_services.sh
@@ -1,11 +1,45 @@
-# does something to the network so the host can connect to localhost
-service iptables stop
+echo ===========================================================================
+echo Start Neo4j Dev server
+echo ===========================================================================
 
-# start neo4j dev server
-/usr/local/share/test_neo4j/bin/neo4j start
+/usr/local/share/neo4j-community-3.0.9/bin/neo4j start
 
-# restart elasticsearch
+echo ===========================================================================
+echo Restart Elasticsearch
+echo ===========================================================================
+
 service elasticsearch restart
 
-# start up redis in the background
+echo ===========================================================================
+echo Start up redis in the background
+echo ===========================================================================
+
 redis-server /etc/redis.conf
+
+echo ===========================================================================
+echo Killing extraneous processes...
+echo ===========================================================================
+
+/etc/init.d/iptables stop
+kill -9 $(cat /tmp/pid/unicorn.pid)
+service nginx stop
+
+echo ===========================================================================
+echo Preparing nginx...
+echo ===========================================================================
+
+cp /vagrant/fenrir/config/fenrir_nginx_conf /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/fenrir_nginx_conf /etc/nginx/sites-enabled/
+
+echo ===========================================================================
+echo Bundling and starting Unicorn
+echo ===========================================================================
+
+# cd /vagrant/fenrir/ && bundle &&
+# bundle exec unicorn -c /vagrant/fenrir/config/unicorn.rb -D
+
+echo ===========================================================================
+echo Starting nginx
+echo ===========================================================================
+
+# service nginx start
